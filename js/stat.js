f1c77e1988cac3a99fa740dab873a8ad89447458
St.Anton <struichkov.anton@gmail.com>
2020-02-05 01:01:20 +0300

    Добавляет script.js  с гистограммой времен участников игры

---
+++ js/stat.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var SHIFT = 10;
+var GAP = 50;
+var MARGIN = 40;
+var barHeight = 150;
+var BAR_WIDTH = 40;
+var BAR_TOP = 90;
+var TEXT_LINE = 250;
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+window.renderStatistics = function (ctx, players, times) {
+  ctx.strokeStyle = 'black';
+  renderCloud(ctx, CLOUD_X + SHIFT, CLOUD_Y + SHIFT, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'white');
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillStyle = '#000000';
+  ctx.fillText('Ура вы победили!', 120, 30);
+  ctx.fillStyle = '#000000';
+  ctx.fillText('Список результатов: ', 120, 50);
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < players.length; i++) {
+    ctx.fillStyle = '#000000';
+    ctx.fillText(
+        Math.floor(times[i]),
+        CLOUD_X + MARGIN + (BAR_WIDTH + GAP) * i,
+        70 + (barHeight - (barHeight * times[i]) / maxTime)
+    );
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    if (players[i] !== 'Вы') {
+      var saturation = Math.floor(101 * Math.random());
+      ctx.fillStyle = 'hsl(235,' + saturation + '%, 27%)';
+    }
+    ctx.fillRect(
+        CLOUD_X + MARGIN + (BAR_WIDTH + GAP) * i,
+        BAR_TOP + (barHeight - (barHeight * times[i]) / maxTime),
+        BAR_WIDTH,
+        (barHeight * times[i]) / maxTime
+    );
+    ctx.fillStyle = '#000000';
+    ctx.fillText(
+        players[i],
+        CLOUD_X + MARGIN + (BAR_WIDTH + GAP) * i,
+        TEXT_LINE
+    );
+  }
+};
